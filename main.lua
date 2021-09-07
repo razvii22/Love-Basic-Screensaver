e703cb55dae07c4867aa5f9944b6f82b981a7bbb
Razvii <razviicaluser@gmail.com>
2021-09-07 21:19:18 +0300

    Made better lol

--- main.lua
+++ main.lua
@@ -6,12 +6,12 @@ function love.load()
   rect.y = 0
   rect.width = 100
   rect.height = 100
-  rect.velx = 1
-  rect.vely = 1
+  rect.velx = 500
+  rect.vely = 500
   rect.w1 = false
   rect.w2 = false
   rect.color = {}
-  rect.color.red = 0
+  rect.color.red = 1
   rect.color.green = 0
   rect.color.blue = 0
   love.window.setFullscreen(true)
@@ -26,36 +26,37 @@ function love.update(dt)
     love.event.quit()
   end
 
-  rect.x = rect.x + rect.velx
-  rect.y = rect.y + rect.vely
+  rect.x = rect.velx * dt + rect.x
+  rect.y = rect.vely * dt + rect.y
 
   if rect.x + rect.width >= love.graphics.getWidth() then
     rect.x = love.graphics.getWidth() - rect.width
     rect.velx = -rect.velx
     rect.w1 = true
-    rect.color.red = math.random(0,1)
-    rect.color.green = math.random(0,1)
-    rect.color.blue = math.random(0,1)
+    rect.color.red = math.random()
+    rect.color.green = math.random()
+    rect.color.blue = math.random()
   elseif rect.x <= 0 then
     rect.velx = -rect.velx
     rect.w1 = true
-    rect.color.red = math.random(0,1)
-    rect.color.green = math.random(0,1)
-    rect.color.blue = math.random(0,1)
+    rect.color.red = math.random()
+    rect.color.green = math.random()
+    rect.color.blue = math.random()
   end
+
   if rect.y + rect.height >= love.graphics.getHeight() then
     rect.y = love.graphics.getHeight() - rect.height
     rect.vely = -rect.vely
     rect.w2 = true
-    rect.color.red = math.random(0,1)
-    rect.color.green = math.random(0,1)
-    rect.color.blue = math.random(0,1)
+    rect.color.red = math.random()
+    rect.color.green = math.random()
+    rect.color.blue = math.random()
   elseif rect.y <= 0 then
     rect.vely = -rect.vely
     rect.w2 = true
-    rect.color.red = math.random(0,1)
-    rect.color.green = math.floor(math.random(0,1))
-    rect.color.blue = math.random(0,1)
+    rect.color.red = math.random()
+    rect.color.green = math.random()
+    rect.color.blue = math.random()
   end
   if rect.w1 and rect.w2 then
      corner = true
@@ -65,19 +66,15 @@ function love.update(dt)
    if corner then
      love.system.openURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
    end
-   if rect.color.blue == 0 and rect.color.green == 0 and rect.color.red == 0 then
-     rect.color.red = 1
-   elseif rect.color.blue == 1 and rect.color.green == 1 and rect.color.red == 1 then
-     rect.color.blue = 0
-   end
 
 end
+
 
 function love.draw()
   love.graphics.setColor(rect.color.red,rect.color.green,rect.color.blue,1)
   love.graphics.print(rect.color.red,1)
-  love.graphics.print(rect.color.green,10)
-  love.graphics.print(rect.color.blue,20)
+  love.graphics.print(rect.color.green,1,10)
+  love.graphics.print(rect.color.blue,1,20)
   love.graphics.rectangle("fill", rect.x,rect.y, rect.width, rect.height)
   love.graphics.print("Wait until it hits the corner :)",love.graphics.getWidth()/2-string.len("Wait until it hits the corner :)"))
 
